0605d8e9f76ab878b2d69ca8e48de6a85f5df4aa
Marshall1604 <luuminhnhat.vn@gmail.com>
2026-09-02 00:08:59 +0700

    Update_NextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+};
 
 export default nextConfig;
